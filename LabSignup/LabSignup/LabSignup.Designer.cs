427ff52653921c298790790cc9441d3ec85c7cc9
justinkeatingdev <justinkeating.dev@gmail.com>
2023-04-23 16:34:17 -0400

    design fixes

--- LabSignup/LabSignup/LabSignup.Designer.cs
+++ LabSignup/LabSignup/LabSignup.Designer.cs
@@ -41,8 +41,6 @@ namespace LabSignup
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -108,31 +106,13 @@ namespace LabSignup
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(71, 329);
+            this.button2.Location = new System.Drawing.Point(71, 304);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(149, 23);
             this.button2.TabIndex = 7;
-            this.button2.Text = "Download Signup Sheet";
+            this.button2.Text = "Download Labs Data Sheet";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(186, 300);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(13, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "0";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(81, 300);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(108, 13);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Students Signed Up: ";
             // 
             // comboBox2
             // 
@@ -156,11 +136,9 @@ namespace LabSignup
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
-            this.ClientSize = new System.Drawing.Size(312, 394);
+            this.ClientSize = new System.Drawing.Size(312, 354);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
@@ -187,8 +165,6 @@ namespace LabSignup
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private Label label4;
-        private Label label5;
         private ComboBox comboBox2;
         private Label label6;
     }

915b3e6d7c4832c5c1963477bd22e127c5e52f2f
justinkeatingdev <justinkeating.dev@gmail.com>
2023-10-01 18:03:56 -0400

    fixing of datageridview1

--- LabSignup/LabSignup/LabSignup.Designer.cs
+++ LabSignup/LabSignup/LabSignup.Designer.cs
@@ -208,6 +208,7 @@ namespace LabSignup
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.FirstName,
